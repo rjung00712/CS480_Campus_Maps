29036448e8acd4704bd559b3207147aea4c722a3
rjung00712 <Richard@Richards-MacBook-Pro-4.local>
2017-04-18 10:05:53 -0700

    initial commit

---
+++ app/src/main/java/cs499android/com/cppcampusmap/MainActivity.java
@@ -0,0 +1,17 @@
+package cs499android.com.cppcampusmap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/cs499android/com/cppcampusmap/Model/Constants.java
@@ -0,0 +1,10 @@
+package cs499android.com.cppcampusmap.Model;
+
+/**
+ * Created by Richard on 4/18/17.
+ */
+
+public class Constants {
+
+    public static final int MY_LOCATION_REQUEST_CODE = 1;
+}
